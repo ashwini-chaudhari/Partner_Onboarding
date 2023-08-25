974fa27508b30c94f2a388da7bfd958b385a99fd
ashwini-chaudhari <chaudhariashwini112@gmail.com>
2023-08-25 10:38:28 +0530

    25 August updated code

--- src/com/TEST.java
+++ src/com/TEST.java
@@ -5,6 +5,8 @@ public class TEST {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test Case 1");
+		
+		System.out.println("TEST CASE2");
 
 	}
 
